6c1120036235ab24d54b03a6ec980b2e341db202
Dolbezhnik <krendelalenkin@gmail.com>
2024-12-03 20:30:58 +0300

    app v1 is done

--- src/App.tsx
+++ src/App.tsx
@@ -67,89 +67,134 @@ function App() {
     }
     return (
         <>
-            <h1>Check weather in your city</h1>
-            <div className="searchBlock">
-                <label>
-                    <input
-                        placeholder="Type your city and press button or Enter key..."
-                        type="text"
-                        className="searchCity"
-                        value={inputCity}
-                        onChange={(evt: any) => updateInputValue(evt)}
-                        onKeyDown={(evt: any) => handleKeyPress(evt)}
-                    />
-                    <button
-                        className="searchBtn"
-                        onClick={() => searchBtnPress()}
-                    >
-                        Search
-                    </button>
-                </label>
-            </div>
+            {/* {!errors && !loading && data.current.condition.text ? (
+                <div
+                    className="dynamic_bg"
+                    style={{ backgroundImage: `url(${rain})` }}
+                >
+                    {/* <img src={rain} alt="rain" /> }
+                </div>            ) : (
+                <></>
+            )} */}
 
-            {errors ? (
-                <h3>ERROR! {errorMessage}</h3>
-            ) : (
-                <div className="card">
-                    {loading ? (
-                        <p>wait, mf</p>
-                    ) : (
-                        <>
-                            <img
-                                src={data.current.condition.icon}
-                                alt="icon_weather"
-                                className="icon"
-                            />
-                            <h3>{data.current.condition.text}</h3>
-                            <h3>
-                                Temperature:{' '}
-                                {data.current.temp_c > 0
-                                    ? `+${data.current.temp_c}`
-                                    : data.current.temp_c}
-                                &deg;C /{' '}
-                                {data.current.temp_f > 0
-                                    ? `+${data.current.temp_f}`
-                                    : data.current.temp_f}
-                                &deg;F
-                            </h3>
-                            <h3>
-                                Feels like:{' '}
-                                {data.current.feelslike_c > 0
-                                    ? `+${data.current.feelslike_c}`
-                                    : data.current.feelslike_c}
-                                &deg;C /{' '}
-                                {data.current.feelslike_f > 0
-                                    ? `+${data.current.feelslike_f}`
-                                    : data.current.feelslike_f}
-                                &deg;F
-                            </h3>
-                            <h3>
-                                Wind: {data.current.wind_mph} mph /{' '}
-                                {data.current.wind_kph} kph &#10;
-                                {data.current.wind_dir}
-                            </h3>
-                            <h3>Humidity: {data.current.humidity}%</h3>
-                            <h3>
-                                Pressure: {data.current.pressure_mb} mb /{' '}
-                                {data.current.pressure_in} in
-                            </h3>
-                            <h3>
-                                Precip: {data.current.precip_mm} mm /{' '}
-                                {data.current.precip_in} in
-                            </h3>
-                            <h3>
-                                Visibility: {data.current.vis_km} km /{' '}
-                                {data.current.vis_miles} miles
-                            </h3>
-                            <h3>Ultraviolet index: {data.current.uv}</h3>
-                            <h3>
-                                Gust: {data.current.gust_mph} mph /{' '}
-                                {data.current.gust_kph} kph
-                            </h3>
-                        </>
-                    )}
+            <div className="app">
+                <h1 className="app__h">Check weather in your city</h1>
+                <div className="searchBlock">
+                    <label className="search__label">
+                        <input
+                            placeholder="Type your city here..."
+                            type="text"
+                            className="search__input"
+                            value={inputCity}
+                            onChange={(evt: any) => updateInputValue(evt)}
+                            onKeyDown={(evt: any) => handleKeyPress(evt)}
+                        />
+                        <button
+                            className="search__btn"
+                            onClick={() => searchBtnPress()}
+                        >
+                            GO
+                        </button>
+                    </label>
                 </div>
-            )}
+
+                {errors ? (
+                    <h3>ERROR! {errorMessage}</h3>
+                ) : (
+                    <>
+                        {loading ? (
+                            <p>Waiting...</p>
+                        ) : (
+                            <>
+                                <div className="info__grid">
+                                    <div className="info__grid-long">
+                                        <div className="info__grid-location">
+                                            <p className="info__text">
+                                                {data.location.name} (
+                                                {data.location.country})
+                                            </p>
+                                            <div className="short-info">
+                                                <div className="img-wrapper">
+                                                    <img
+                                                        src={
+                                                            data.current
+                                                                .condition.icon
+                                                        }
+                                                        alt="icon_weather"
+                                                        className="icon"
+                                                    />
+                                                </div>
+                                                <p className="info__text">
+                                                    {
+                                                        data.current.condition
+                                                            .text
+                                                    }
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="info-temp">
+                                            <p className="info__text">
+                                                Temperature:{' '}
+                                                {data.current.temp_c > 0
+                                                    ? `+${data.current.temp_c}`
+                                                    : data.current.temp_c}
+                                                &deg;C /{' '}
+                                                {data.current.temp_f > 0
+                                                    ? `+${data.current.temp_f}`
+                                                    : data.current.temp_f}
+                                                &deg;F
+                                            </p>
+                                            <p className="info__text">
+                                                Feels like:{' '}
+                                                {data.current.feelslike_c > 0
+                                                    ? `+${data.current.feelslike_c}`
+                                                    : data.current.feelslike_c}
+                                                &deg;C /{' '}
+                                                {data.current.feelslike_f > 0
+                                                    ? `+${data.current.feelslike_f}`
+                                                    : data.current.feelslike_f}
+                                                &deg;F
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="info__grid-short">
+                                        <p className="info__text">
+                                            Wind: {data.current.wind_mph} mph /{' '}
+                                            {data.current.wind_kph} kph &#10;
+                                            {data.current.wind_dir}
+                                        </p>
+                                        <p className="info__text">
+                                            Humidity: {data.current.humidity}%
+                                        </p>
+                                        <p className="info__text">
+                                            Pressure: {data.current.pressure_mb}{' '}
+                                            mb / {data.current.pressure_in} in
+                                        </p>
+                                        <p className="info__text">
+                                            Precip: {data.current.precip_mm} mm
+                                            / {data.current.precip_in} in
+                                        </p>
+                                    </div>
+                                    <div className="info__grid-short">
+                                        <p className="info__text">
+                                            Visibility: {data.current.vis_km} km
+                                            / {data.current.vis_miles} miles
+                                        </p>
+
+                                        <p className="info__text">
+                                            Gust: {data.current.gust_mph} mph /{' '}
+                                            {data.current.gust_kph} kph
+                                        </p>
+                                        <p className="info__text">
+                                            Ultraviolet index: {data.current.uv}
+                                        </p>
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </>
+                )}
+            </div>
         </>
     );
 }
